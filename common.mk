8b8fb1bb149946b5b39a4d11d3a8880ee098f7b0
StarkDroid <gamerprince.exp@gmail.com>
2016-03-31 04:26:02 +0200

    bringup zos

--- common.mk
+++ common.mk
@@ -18,29 +18,29 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/zos/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Include LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/zos/overlay/dictionaries
 
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosp/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+    vendor/zos/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/zos/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/zos/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # Init file
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.cm.rc
+    vendor/zos/prebuilt/common/etc/init.local.rc:root/init.cm.rc
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/zos/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/zos/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
    
 # Layers Manager
 PRODUCT_COPY_FILES += \
-vendor/aosp/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
+vendor/zos/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
 
 # Layers Backup
 PRODUCT_COPY_FILES += \
@@ -81,6 +81,15 @@ PRODUCT_PACKAGES += \
     strace \
     Terminal
 
+# Required Packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    CellBroadcastReceiver \
+    su \
+    Snap \
+    Eleven \
+    CMFileManager 
+
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
     libffmpeg_extractor \
@@ -102,24 +111,24 @@ PRODUCT_PACKAGES += \
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/zos/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/zos/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
 # Overlays & Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += \
-	vendor/aosp/overlay/common \
-	vendor/aosp/overlay/dictionaries
+	vendor/zos/overlay/common \
+	vendor/zos/overlay/dictionaries
 
 # Proprietary latinime libs needed for Keyboard swyping
 ifneq ($(filter arm64,$(TARGET_ARCH)),)
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/zos/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 else
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+    vendor/zos/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
 endif
 
 # by default, do not update the recovery with system updates
