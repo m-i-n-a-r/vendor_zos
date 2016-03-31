hashroot/vendor_orion

Layers: dynamic backup script

config/common.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 vendor/orion/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
 
+# Layers Backup
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/bin/71-layers.sh:system/addon.d/71-layers.sh
+ 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 

m-i-n-a-r/vendor_zos

Conflicts:
	config/common.mk

common.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
 vendor/aosp/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
 
+# Layers Backup
+PRODUCT_COPY_FILES += \
+    vendor/zos/prebuilt/common/bin/71-layers.sh:system/addon.d/71-layers.sh
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
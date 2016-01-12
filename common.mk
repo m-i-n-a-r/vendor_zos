37a2b216c0e1a499621de4972dcec3c474b62579
George G <kreach3r@users.noreply.github.com>
2016-01-12 18:57:11 -0500

    Layers: dynamic backup script
    Conflicts:
    	config/common.mk

--- common.mk
+++ common.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
 vendor/aosp/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
 
+# Layers Backup
+PRODUCT_COPY_FILES += \
+    vendor/zos/prebuilt/common/bin/71-layers.sh:system/addon.d/71-layers.sh
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
